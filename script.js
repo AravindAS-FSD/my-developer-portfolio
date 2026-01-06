4d854620a6a64df7f61326d7f466c49e55266e4c
AravindAS-FSD <aravindas715@gmail.com>
2026-01-06 20:24:36 +0530

    feat: add responsive flexbox header

--- script.js
+++ script.js
@@ -1,10 +1,24 @@
-// Select the element
-const hireButton = document.getElementById('hireBtn');
+// // Select the element
+// const hireButton = document.getElementById('hireBtn');
 
-// Define the action
-function showMessage() {
-    alert("Great choice! Let's build something amazing together.");
-}
+// // defining the action and adding it
+// hireButton.addEventListener('click', () => {
+//     alert("Great choice! Let's build something amazing together.");
+//     window.location.href = "mailto:asaravind213@gmail.com";
+// });
 
-// Listen for the click
-hireButton.addEventListener('click', showMessage);
+// Action without alert
+document.getElementById('hireBtn').addEventListener('click', () => {
+    window.location.href = "mailto:asaravind213@gmail.com";
+});
+
+// moblie menu toogle
+const menuToggle = document.querySelector('.menu-toggle');
+const nav = document.querySelector('nav');
+const navLinks = document.querySelectorAll('nav a, nav button');
+
+menuToggle.addEventListener('click', () => {
+    nav.classList.toggle('active');
+    document.body.style.overflow = nav.classList.contains('active') ? 'hidden' : '';
+    menuToggle.innerHTML = nav.classList.contains('active') ? '&#10005' : '&#9776'; 
+});
